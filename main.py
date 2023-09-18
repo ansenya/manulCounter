dc251bac36e5298e3b4bd768181a0d28adec8ba0
ansenya <ars.anoshin@gmail.com>
2023-09-18 23:25:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import telebot
+import re
+
+bot = telebot.TeleBot('6561490280:AAFLZgHFLsHEbJGCnBo17Yec5bFhJI35i1c')
+
+numManuls = 0
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def reply_to_hello(message):
+    global numManuls
+
+    manulPattern = get_manul_pattern(message.text.lower())
+    countPattern = get_count_pattern(message.text.lower())
+
+    if countPattern:
+        for i in range(int(countPattern[1])):
+            numManuls += 1
+            bot.send_message(message.chat.id, "{} манулов".format(numManuls))
+
+    if manulPattern:
+        if int(manulPattern[0]) < numManuls:
+            bot.send_message(message.chat.id, "это число меньше предыдущего. иди нахуй.")
+            return
+        numManuls += 1
+        bot.send_message(message.chat.id, "{} манулов".format(numManuls))
+
+    if message.text.lower() == "манул":
+        numManuls += 1
+        bot.send_message(message.chat.id, "{} манулов".format(numManuls))
+
+
+def get_manul_pattern(s):
+    pattern = r"([0-9]*) ([а-я]*)"
+    matches = re.search(pattern, s, re.IGNORECASE)
+
+    if matches:
+        number = matches.group(1)
+        manul = matches.group(2)
+        if manul == "манулов" or manul == "манул":
+            return [number, manul]
+        return 0
+    else:
+        return 0
+
+
+def get_count_pattern(s):
+    pattern = r"([а-яА-я]*) ([0-9]*) ([а-яА-я]*)"
+    matches = re.search(pattern, s, re.IGNORECASE)
+
+    if matches:
+        check = matches.group(1)
+        count = matches.group(2)
+        manul = matches.group(3)
+
+        if check.lower() == "отсчитай" and manul == "манулов" or "манул":
+            return [check, count, manul]
+        return 0
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
